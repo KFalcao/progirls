fb0ebecbdc2d1740f7fb56a8913df2515448c27f
KFalcao <karollinefalcao@gmail.com>
2025-01-29 20:15:49 -0300

    :sparkles: feat: pagina sobre nos

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -7,7 +7,6 @@ export default function sobre() {
         <p>A Programmer Girl começou em 2022 como uma iniciativa de Raquel Maia (@maia_dev) para compartilhar conhecimentos em programação. Hoje, é uma comunidade com mais de 150 mulheres no WhatsApp, LinkedIn e Instagram, apoiada pelas administradoras Maria, Sabrinne, Joana e Fufu. O grupo promove inclusão, suporte emocional, aprendizado técnico, idiomas, troca de experiências e oportunidades de trabalho, criando um ambiente seguro e acolhedor para mulheres de todas as origens crescerem juntas na área de tecnologia.</p>
         </div>
         <div>
-          <img src="" alt="" />
         </div>
       </section>
     </>
